187264045d09cc500d1466a707f1d70f392a8e3d
Nebil Nej <nejmudinn@gmail.com>
2023-09-05 15:05:34 +0300

    added content.php

---
+++ template-parts/content-none.php
@@ -0,0 +1,14 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header">
+        <h1>
+            <?php esc_html_e('404', 'wphierarchy'); ?>
+        </h1>
+    </header>
+    <div class="entry-content">
+        <p>
+        <h1>
+            <?php esc_html_e('Sorry! No content found', 'wphierarchy'); ?>
+        </h1>
+        </p>
+    </div>
+</article>
